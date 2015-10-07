e53999a48b9d2c13c13b933e004806f1a0cb3aec
TrustFuture <yuanlinhuaxue@qq.com>
2015-10-07 20:06:09 +0800

    correct missed conditions
    to make the programming more acurate

--- ruby_intro.rb
+++ ruby_intro.rb
@@ -1,16 +1,13 @@
-# When done, submit this entire file.
 
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
   sum=0
   arr.each{|x|sum=sum+x}
   return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
   sum = 0
   if arr.length>1
     srt_arr=arr.sort
@@ -24,56 +21,51 @@ def max_2_sum arr
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
   if 0==arr.length
-  	if 0==n
-  		return true
-  	else
-  		return false
-  	end
+    if 0==n
+      return true
+    else
+      return false
+    end
   else
-  	if 1==arr.length
-  		if arr[0]==n
-  			return true
-  		else
-  			return false
-  		end
-  	else
-  		symb=0
-  		arr.each{|x|
-  			flag=n-x
-  			arr.each{|y|
-  				# 寻找 配对数flag，并且两个求和的数不能相同
-  				if flag==y&&x!=y
-  					symb=1
-  				end
-  			}
-  		}
-  	end
-  	if symb==1
-  		return true
-  	else
-  		return false
-  	end
+    if 1==arr.length
+      if arr[0]==n
+        return true
+      else
+        return false
+      end
+    else
+      symb=0
+      arr.each{|x|
+        flag=n-x
+        arr.each{|y|
+          # 寻找 配对数flag，并且两个求和的数不能相同
+          if flag==y&&x!=y
+            symb=1
+          end
+        }
+      }
+    end
+    if symb==1
+      return true
+    else
+      return false
+    end
   end
 end
-
 
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
   return "Hello, "<<name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
   return (/\A[\p{L}&&[^AEIOUaeiou]]/ =~ s) != nil
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
   if /\A0\Z/ =~ s
     return true
   else
@@ -82,29 +74,48 @@ def binary_multiple_of_4? s
 end
 
 
-
-
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
-	def initialize(isbn,price)
-		@ISBN=isbn
-		@PRICE=price
-    if @ISBN==""||@PRICE<=0.0
+  def initialize(isbn,price)
+    if isbn==""||price<=0.0
+      @flag=nil
       raise ArgumentError
     end
   rescue =>err
     puts err
-    puts "Plese input proper ISBN (cannot be null) and price (more than 0)!"
-	end
-	def price_as_string()
-    if @ISBN==""||@PRICE<=0.0
+  else
+    @isbn=isbn
+    @price=price
+  end
+
+  def isbn
+    if @flag
       raise ArgumentError
     end
   rescue => err
     puts err
   else
-		puts "$%.2f" % @PRICE
-	end
-end
+    @isbn
+  end
+
+  def price
+    if @flag
+      raise ArgumentError
+    end
+  rescue => err
+    puts err
+  else
+    @price
+  end
+
+  def price_as_string()
+    if @flag
+      raise ArgumentError
+    end
+  rescue => err
+    puts err
+  else
+    puts "$%.2f" % @price
+  end
+end 
